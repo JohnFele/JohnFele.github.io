9565eafe09b0dbf2e9d0299b1a91e29610c88c11
John Siphamandla Fele <fele476@gmail.com>
2025-02-15 11:35:23 +0200

    Added separate script to handle nav highlighting when scrolling on home page

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,3 @@
-const navbar = document.getElementById('navbar');
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll("nav a");
-
-function toggleSidebar() {
-  if(navbar.classList.contains('show-sidebar')){
-    navbar.classList.remove('show-sidebar');
-  } else {
-    navbar.classList.add('show-sidebar');
-  }
-}
-
-function changeActiveLink() {
-  let scrollPosition = window.scrollY;
-
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop - 250;
-    const sectionHeight = section.offsetHeight;
-    const sectionId = section.getAttribute('id');
-
-    if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-      navLinks.forEach((link) => link.classList.remove("active-link"));
-      document
-        .querySelector(`nav a[href="#${sectionId}"]`)
-        ?.classList.add("active-link");
-    }
-  });
-}
-
-window.addEventListener("scroll", changeActiveLink);
-
 const elements = document.querySelectorAll(".hidden");
   
 const observer = new IntersectionObserver((entries) => {
