2630447d820c364262a7be9ad5b8304f671d4b86
John Siphamandla Fele <fele476@gmail.com>
2025-02-21 23:13:08 +0200

    Added new stuff to site

---
+++ src/js/projects.js
@@ -0,0 +1,29 @@
+const data = fetch("../js/projects.json")
+  .then(response => response.json())
+  .then(projectData => addProjectData(projectData))
+  .catch(error => console.error("Error fetching data:", error));
+
+const addProjectData = (projects) => {
+  const projectsContainer = document.querySelector(".projects-grid");
+  projects.forEach((project) => {
+    const projectCard = document.createElement("div");
+    projectCard.classList.add("project-card");
+
+    projectCard.innerHTML = `
+            <img src="../assets/images/projects/${project.image}" alt="Project 1">
+            <h3>${project.title}</h3>
+            <p>
+              ${project.description}
+            </p>
+            <div class="project-links">
+              <a href="${project.github}" target="_blank" class="project-link">
+                <i class="fa-brands fa-github"></i> GitHub
+              </a>
+              <a href="${project.link}" target="_blank" class="project-link">
+                <i class="fa-solid fa-globe"></i> Live Demo
+              </a>
+            </div>
+            `;
+    projectsContainer.appendChild(projectCard);
+  });
+};
